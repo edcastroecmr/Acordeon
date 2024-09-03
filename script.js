5e196f4cd42797138bf4a4791325ed7c8bb0b662
Ed Rodrigues <edcastroecmr@yahoo.com>
2024-09-03 01:17:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-const pegarSetaLi = document.querySelectorAll(".item-lista");
+const pegarLi = document.querySelectorAll(".item-lista");
+const pegarTextoOculto = document.querySelectorAll(".texto")
 
-pegarSetaLi.forEach((item) => {
-    item.addEventListener("click", () => {
-        // Verifica se o item clicado já está aberto
-        const isOpen = item.classList.contains("seta-aberta");
 
-        // Primeiro, esconde todos os textos e fecha todas as setas
-        const todosTextos = document.querySelectorAll(".texto");
-        todosTextos.forEach((texto) => {
-            texto.classList.remove("mostrar");
-        });
 
-        pegarSetaLi.forEach((li) => {
-            li.classList.remove("seta-aberta");
-        });
+pegarLi.forEach((lis) =>{
+    lis.addEventListener("click", ()=>{
+    let setaAberta = lis.classList.contains("seta-aberta")
 
-        // Se o item não estava aberto, exibe o texto e abre a seta correspondente
-        if (!isOpen) {
-            const textoAssociado = item.querySelector(".texto");
-            if (textoAssociado) {
-                textoAssociado.classList.add("mostrar");
-            }
-            item.classList.add("seta-aberta");
-        }
-    });
-});
+
+    pegarTextoOculto.forEach((textinho)=>{textinho.classList.remove("mostrar")})
+    pegarLi.forEach((titulos)=>{titulos.classList.remove("seta-aberta")})
+
+     
+    if (!setaAberta) {
+         const textoAssociado = lis.querySelector(".texto");
+         if (textoAssociado) {
+            textoAssociado.classList.add("mostrar");
+         }
+         lis.classList.add("seta-aberta");
+     }
+     
+
+    })
+})
+
+
+
+
+
+// pegarTextoOculto.forEach((textinhos)=>{textinhos.classList.remove("mostrar") })
